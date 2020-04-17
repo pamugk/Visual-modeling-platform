ea1cede8580a946d2b63b6cac9135a3c303c8310
Alexander <pamugk@gmail.com>
2020-04-17 22:28:19 +0500

    Provided basic functional for creation of models

--- dsm-platform-lib/src/main/kotlin/ru/psu/DsmPlatform.kt
+++ dsm-platform-lib/src/main/kotlin/ru/psu/DsmPlatform.kt
@@ -1,13 +1,24 @@
 package ru.psu
 
 import ru.psu.generator.DslDefGenerator
+import ru.psu.graphs.MLGraph
+import ru.psu.model.Model
 import ru.psu.repository.ModelRepository
-import ru.psu.transformator.ModelTransformator
+import ru.psu.transformer.ModelTransformer
 import ru.psu.validator.ModelValidator
+import java.util.*
 
 class DsmPlatform(
         var defGenerator:DslDefGenerator,
         var validator:ModelValidator,
-        var transforator:ModelTransformator,
+        var transforator:ModelTransformer,
         var repository:ModelRepository
-)
+) {
+    fun createMetamodel(name:String = "Metamodel", description:String = "") =
+        Model(null, UUID.randomUUID(), name, description,
+                MLGraph(null, UUID.randomUUID(), null))
+
+    fun createModel(metamodel:Model, name:String = "Model", description:String = "") =
+            Model(metamodel.id, UUID.randomUUID(), name, description,
+                    MLGraph(null, UUID.randomUUID(), metamodel.root.id))
+}
