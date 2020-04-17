25a016651f105332eb7e8c8f3e3693239844657d
Alexander <pamugk@gmail.com>
2020-04-17 16:03:01 +0500

    Slightly updated class model

--- metalanguage-lib/src/main/kotlin/ru/psu/graphs/MLGraph.kt
+++ metalanguage-lib/src/main/kotlin/ru/psu/graphs/MLGraph.kt
@@ -1,17 +1,14 @@
 package ru.psu.graphs
 
-import ru.psu.constructs.MLConstruct
 import java.util.*
 import kotlin.collections.HashSet
 
 //Класс графа в MetaLanguage 1.1
 class MLGraph(
-        parentId:UUID?, //Идентификатор конструкции-родителя
-        id: UUID, //Идентификатор
-        name:String, //Название
-        prototypeId:UUID?, //Прототип (если null, то это конструкция в метамодели)
-        innerStructure:UUID? //Id графа, описывающего внутреннюю структуру (если есть)
-): MLConstruct(parentId, id, name, prototypeId, innerStructure) {
+        val parentId:UUID?, //Идентификатор конструкции-родителя
+        val id: UUID, //Идентификатор
+        val prototypeId:UUID? //Прототип (если null, то это конструкция в метамодели)
+) {
     val entities:MutableSet<UUID> = HashSet() //Id сущностей в графе
     val ports:MutableSet<UUID> = HashSet() //Id портов в графе
     val relations:MutableSet<UUID> = HashSet() //Id отношений в графе
--- model-lib/src/main/kotlin/ru/psu/model/Model.kt
+++ model-lib/src/main/kotlin/ru/psu/model/Model.kt
@@ -9,11 +9,13 @@ import kotlin.collections.HashMap
 
 //Класс для описания модели и метамодели
 class Model(
+        val prototypeId: UUID?, //Id прототипа (null в случае метамодели, UUID метамодели в случае модели)
         val id: UUID, //Id модели
         var name:String, //Название модели
         var description:String, //Описание модели
         val root:MLGraph //Корневой граф
 ) {
     val views: MutableList<View> = ArrayList() //Множество графических представлений модели/метамодели
+    val graphs: MutableMap<UUID, MLGraph> = HashMap() //Множество графов
     val constructs: MutableMap<UUID, MLConstruct> = HashMap() //Множество конструкций
 }
